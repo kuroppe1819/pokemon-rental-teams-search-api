d7791667c0c483343d7deb2d21c5271a8bbb3f75
kuroppe <17245737+kuroppe1819@users.noreply.github.com>
2023-01-07 15:56:36 +0900

    environment variable setting.

--- search/src/twitter.ts
+++ search/src/twitter.ts
@@ -2,10 +2,7 @@ import Client from 'twitter-api-sdk';
 
 export const tweetsRecentSearch = async (searchText: string, nextToken?: string) => {
     // TODO: git にアップロードしちゃったからAPIキー発行しなおしてね
-    const BEARER_TOKEN =
-        'AAAAAAAAAAAAAAAAAAAAAIKSkwEAAAAAgxhnU2vb1sYfVt5nfFmivG%2F1p9U%3Dno4T3MWdM694ScyIIf7mcKBa0GuTUYQmlWh5bTXEGppNARgFJZ';
-    // TODO: process.env.TWITTER_BEARER_TOKEN
-    const client = new Client(BEARER_TOKEN);
+    const client = new Client(process.env.TWITTER_BEARER_TOKEN!);
     const tweet = await client.tweets.tweetsRecentSearch({
         query: `${searchText} has:images -is:retweet`,
         max_results: 10,
